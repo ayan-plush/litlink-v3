128fd177e51adbacb4a3d917c21114f9fbd23781
ayan-plush <ayankhajuria2005@gmail.com>
2025-02-12 12:22:46 +0530

    fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const http = require('http')
 const sellerModel = require('./models/sellerModel')
 const server = http.createServer(app)
 app.use(cors({
-    origin : ['https://litlink-frontend.onrender.com/'],
+    origin : ['https://litlink-frontend.onrender.com','http://localhost:5173'],
     credentials : true
 }))
 
