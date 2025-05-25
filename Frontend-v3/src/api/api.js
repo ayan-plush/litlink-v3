15d34f6c83b27802e32257c78211a6bc56fcc1b9
ayan-plush <ayankhajuria2005@gmail.com>
2025-05-25 20:02:29 +0530

    fix: api.j readded

---
+++ Frontend-v3/src/api/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL : 'http://localhost:5000/api'
+})
+
+// https://litlink-backend.onrender.com/api http://localhost:5000/api
+
+export default api 
