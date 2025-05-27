af5a67080223b943d22c751cb448490afb66a650
ayan-plush <ayankhajuria2005@gmail.com>
2025-05-27 08:43:36 +0530

    fix this issue

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -6,7 +6,7 @@ const sellerModel = require('../models/sellerModel');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: `${process.env.BACKEND_URL}/api/seller/google/callback`
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
