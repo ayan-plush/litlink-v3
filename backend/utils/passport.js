1c4873e6f8fa559170d3798a9dc4d1d1dd8031c3
ayan-plush <ayankhajuria2005@gmail.com>
2025-05-27 08:29:30 +0530

    variable callback url

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -6,7 +6,7 @@ const sellerModel = require('../models/sellerModel');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/seller/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
